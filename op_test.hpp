740048d35665fab30c6bae82b5657de109408c8a
gokirstengo <kirstengo568@gmail.com>
2021-04-14 23:14:57 -0700

    Added test cases for Op class

--- op_test.hpp
+++ op_test.hpp
@@ -5,9 +5,35 @@
 
 #include "op.hpp"
 
-TEST(OpTest, OpEvaluateNonZero) {
-    Op* test = new Op(8);
-    EXPECT_EQ(test->evaluate(), 8);
+//Op class tests
+TEST(OpTest, OpEvaluatePositive) {
+    Op* test1 = new Op(8);
+    EXPECT_DOUBLE_EQ(test1->evaluate(), 8);
 }
+
+TEST(OpTest, OpEvaluateNegative) {
+    Op* test2 = new Op(-8);
+    EXPECT_DOUBLE_EQ(test2->evaluate(), -8);
+}
+
+TEST(OpTest, OpEvaluateZero) {
+    Op* test3 = new Op(0);
+    EXPECT_DOUBLE_EQ(test3->evaluate(), 0);
+}
+
+TEST(OpTest, OpStringPositive) {
+    Op* test4 = new Op(1.1);
+    EXPECT_EQ(test4->stringify(), "1.100000");
+}
+
+TEST(OpTest, OpStringNegative) {
+    Op* test5 = new Op(-1.1);
+    EXPECT_EQ(test5->stringify(), "-1.100000");
+}
+TEST(OpTest, OpStringZero) {
+    Op* test6 = new Op(0);
+    EXPECT_EQ(test6->stringify(), "0.000000");
+}
+
 
 #endif //__OP_TEST_HPP__
